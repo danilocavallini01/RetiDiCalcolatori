acfda17b30b28e1d1563584f9ff8ba6604a40760
Danilo <danilo.cavallini.01@gmail.com>
2022-10-26 17:21:04 +0200

    Update PutFileServer.java

--- es2/PutFileServer.java
+++ es2/PutFileServer.java
@@ -14,13 +14,6 @@ public class PutFileServer {
     public static void main(String args[]) {
         ServerSocket serverSocket = null;
         Socket socket = null;
-        String file = null;
-        
-        long dim;
-
-        DataInputStream inSock = null;
-        DataOutputStream outSock = null;
-        FileOutputStream outFile = null;
     
         try {
             serverSocket = new ServerSocket(port);
@@ -32,58 +25,72 @@ public class PutFileServer {
         }
 
         while (true) {
-            System.out.println("PutFileServerSeq: avviato ");
+            System.out.println("PutFileServer: avviato ");
             System.out.println("Creata la server socket: " + serverSocket);
 
             try {
                 socket = serverSocket.accept();
-
-                inSock = new DataInputStream(socket.getInputStream());
-                outSock = new DataOutputStream(socket.getOutputStream());
-
-                while (!socket.isClosed() ) {
-                    file = inSock.readUTF();
-                    System.out.println(">------------------------------------");
-                   
-                    System.out.println("Ricevuto dal client file: " + file);
-
-                    if (new File(file).exists()) {
-                        outSock.writeUTF("salta");
-                        System.out.println("File gia presente: salta");
-                    } else {
-                        outSock.writeUTF("attiva");
-
-                        dim = inSock.readLong();
-
-                        System.out.println("File non presente: attiva");
-                        System.out.println("File di dimensione: " + dim);
-
-                        outFile = new FileOutputStream(file);
-                        FileUtility.trasferisci_a_byte_file_binario(inSock,new DataOutputStream(outFile), dim);
-                        outFile.close();
-                        System.out.println("Finito di trasferire file: " + file);
-                    }
-                }
-                
-
-                socket.close();
-                
-            } catch (EOFException e) {
-                System.out.println("Ricezione eseguita con successo da un host");
-                System.out.println("##########################################");
-                continue;
-            } catch (NumberFormatException e) {
-                System.err.println("Problemi nella socket: " + e.getMessage());
+                new PutFileServerThread(socket).start();
+            } catch (IOException e) {
                 e.printStackTrace();
                 continue;
-            } catch (IOException e ) {
-                System.err.println("Problemi nella socket: " + e.getMessage());
-                e.printStackTrace();
-                continue;
-            }
-            
+            }  
         }
-
     }
 
+}
+
+class PutFileServerThread extends Thread {
+    private Socket socket;
+    private String file;  
+    private long dim;
+
+    private DataInputStream inSock = null;
+    private DataOutputStream outSock = null;
+    private FileOutputStream outFile = null;
+
+    public PutFileServerThread(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            inSock = new DataInputStream(socket.getInputStream());
+            outSock = new DataOutputStream(socket.getOutputStream());
+
+            while (!socket.isClosed() ) {
+                file = inSock.readUTF();
+                System.out.println(">------------------------------------");
+                
+                System.out.println("Ricevuto dal client file: " + file);
+
+                if (new File(file).exists()) {
+                    outSock.writeUTF("salta");
+                    System.out.println("File gia presente: salta");
+                } else {
+                    outSock.writeUTF("attiva");
+
+                    dim = inSock.readLong();
+
+                    System.out.println("File non presente: attiva");
+                    System.out.println("File di dimensione: " + dim);
+
+                    outFile = new FileOutputStream(file);
+                    FileUtility.trasferisci_a_byte_file_binario(inSock,new DataOutputStream(outFile), dim);
+                    outFile.close();
+                    System.out.println("Finito di trasferire file: " + file);
+                }
+            }
+
+        } catch (EOFException e) {
+            System.out.println("Ricezione eseguita con successo da un host");
+            System.out.println("##########################################");
+        } catch (NumberFormatException e) {
+            System.err.println("Problemi nella socket: " + e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e ) {
+            System.err.println("Problemi nella socket: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
